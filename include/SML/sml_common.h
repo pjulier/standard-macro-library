e3930e0fc9596ac7354d873e472139375c169098
pjulier <114404387+pjulier@users.noreply.github.com>
2025-12-02 21:26:44 +0100

    Fixed missing include guards

--- include/SML/sml_common.h
+++ include/SML/sml_common.h
@@ -1,3 +1,6 @@
+#ifndef INCLUDE_SML_COMMON_H
+#define INCLUDE_SML_COMMON_H
+
 #include <string.h> /* for memset */
 
 /*
@@ -7,3 +10,5 @@
 #define SML_ZERO(x)  (memset(&(x), 0, sizeof(x)))
 #define SML_ZEROA(x) (memset(x, 0, sizeof(x)))
 #define SML_ZEROP(x) (memset(x, 0, sizeof(*x)))
+
+#endif /* INCLUDE_SML_COMMON_H */
